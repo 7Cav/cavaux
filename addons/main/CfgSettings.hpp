71ae486cb7f1591912ad2acdece911a7d705afd2
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-07-20 23:33:40 +0200

    fixed versioning

--- addons/main/CfgSettings.hpp
+++ addons/main/CfgSettings.hpp
@@ -1,7 +1,7 @@
 class CfgSettings {
     class CBA {
         class Versioning {
-            class CavAux {
+            class cavaux {
                 class dependencies {
                     //Mod will hard exit if this is missing
                     CBA[] = {"cba_main", REQUIRED_CBA_VERSION, "(true)"};
